4bb038f14397a47087f84e3347d1187f701c30f0
Json Alzate <developer.alzate@gmail.com>
2022-10-06 17:01:51 -0500

    ✨Take pictures

--- src/app/pages/questions/components/new-question/new-question.component.ts
+++ src/app/pages/questions/components/new-question/new-question.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
-import { ModalController } from '@ionic/angular';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import { ModalController, AlertController } from '@ionic/angular';
 
 
 @Component({
@@ -11,12 +11,15 @@ import { ModalController } from '@ionic/angular';
 })
 export class NewQuestionComponent implements OnInit {
 
+  photo1: string;
+  photo2: string;
   tags = ['Automática', 'Feminizada', 'Regular', 'InDoor', 'Exterior', 'Vegetación', 'Floración', 'Poda', 'Plaga'].sort();
   form: FormGroup;
 
   constructor(
     private modalController: ModalController,
     private formBuilder: FormBuilder,
+    private alertController: AlertController
   ) {
 
     this.buildForm();
@@ -44,6 +47,32 @@ export class NewQuestionComponent implements OnInit {
       description: ['', [Validators.required, Validators.maxLength(1000)]],
       tags: []
     });
+  }
+
+
+
+  async takePhoto(photoOption: number) {
+
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Base64
+    });
+
+    // image.webPath will contain a path that can be set as an image src.
+    // You can access the original file using image.path, which can be
+    // passed to the Filesystem API to read the raw data of the image,
+    // if desired (or pass resultType: CameraResultType.Base64 to getPhoto)
+    const imageUrl = image.base64String;
+
+    switch (photoOption) {
+      case 1:
+        this.photo1 = 'data:image/jpeg;base64,' + imageUrl;
+        break;
+      case 2:
+        this.photo2 = 'data:image/jpeg;base64,' + imageUrl;
+        break;
+    }
   }
 
   onSubmit() {
