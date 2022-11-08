4fc839596fbd44b65e60a942fdd5222cac84f02c
json-alzate <developer.alzate@gmail.com>
2022-11-08 13:21:54 -0500

    🚧on calendar component

--- src/app/pages/plants/components/new-plant/new-plant.component.ts
+++ src/app/pages/plants/components/new-plant/new-plant.component.ts
@@ -1,7 +1,21 @@
+//core and third party libraries
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-
 import { ModalController } from '@ionic/angular';
+
+// rxjs
+
+// states
+
+// actions
+
+// selectors
+
+// models
+
+// services
+
+// components
 
 
 @Component({
@@ -12,12 +26,17 @@ import { ModalController } from '@ionic/angular';
 export class NewPlantComponent implements OnInit {
 
   form: FormGroup;
+  types = ['Automática', 'Feminizada', 'Regular'].sort();
+
+  cover: string;
 
 
   constructor(
     private modalController: ModalController,
     private formBuilder: FormBuilder
-  ) { }
+  ) {
+    this.buildForm();
+  }
 
   get tittleField() {
     return this.form.get('name');
@@ -28,10 +47,14 @@ export class NewPlantComponent implements OnInit {
   buildForm() {
     this.form = this.formBuilder.group({
       name: ['', [Validators.required, Validators.maxLength(50)]],
-      description: ['', [Validators.required, Validators.maxLength(100)]],
+      description: ['', [Validators.maxLength(800)]],
       type: ['', [Validators.required, Validators.maxLength(100)]],
       variety: ['', [Validators.required, Validators.maxLength(100)]]
     });
+  }
+
+  launchCalendar(event: any) {
+    console.log('launchCalendar');
   }
 
 
